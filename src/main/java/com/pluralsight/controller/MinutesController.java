404d43ab826927e077c1fb41c0ee0fe1d914d8a6
MaxD <max.amb@mail.ru>
2017-05-15 16:49:04 +0300

    500

--- src/main/java/com/pluralsight/controller/MinutesController.java
+++ src/main/java/com/pluralsight/controller/MinutesController.java
@@ -14,14 +14,15 @@ public class MinutesController {
 
         System.out.println("exercise: " + exercise.getMinutes());
 
-        return "redirect:addMinutes.html";
+        //return "redirect:addMinutes.html";
+        return "addMinutes";
     }
 
-    @RequestMapping(value = "/addMoreMinutes")
+    /*@RequestMapping(value = "/addMoreMinutes")
     public String addMoreMinutes(@ModelAttribute ("exercise")Exercise exercise) {
 
         System.out.println("exercising: " + exercise.getMinutes());
 
         return "addMinutes";
-    }
+    }*/
 }
